f1daae9d632d01b84e2f79542aadd20fc0abd838
ADITYA2777 <ajadityajain01@gmail.com>
2023-11-23 04:44:25 -0800

    Setup Project

---
+++ src/components/Body.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Login from "./Login";
+import Browse from "./Browse";
+import FooterPage from "./FooterPage";
+
+const Body = () => {
+  return (
+    <>
+      <Login />
+      <Browse />
+      <FooterPage />
+    </>
+  );
+};
+
+export default Body;
